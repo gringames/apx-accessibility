3ef85770c5aa9b6523b7925a187642ab082a809e
kerstin-pc <kerstin@irox-games.com>
2026-01-07 18:25:33 +0100

    support recursive hiding

--- src/apx-accessibility/addons/apx-accessibility/clear_channels/background_simplifier.gd
+++ src/apx-accessibility/addons/apx-accessibility/clear_channels/background_simplifier.gd
@@ -3,7 +3,11 @@ extends Node
 class_name BackgroundSimplifier
 
 @export var complex_group_names: Array[String] = ["ComplexBackgroundElement", "MovingBackgroundElement"]
-@export var update_group_during_runtime: bool = false
+@export var update_groups_during_runtime: bool = false
+@export var hide_recursively: bool = false
+
+const HIDE_METHOD: String = "hide"
+const SHOW_METHOD: String = "show"
 
 var complex_nodes: Array[Node]
 
@@ -27,28 +31,21 @@ func _update_complex_nodes() -> void:
 
 func _on_reduce_complexity_changed(new_reduce_complexity: bool) -> void:
 	# search for nodes each update if requested
-	if update_group_during_runtime:
+	if update_groups_during_runtime:
 		_update_complex_nodes()
-	if complex_nodes.is_empty():
-		return
+	if complex_nodes.is_empty(): return
 	_change_visibility_of_group(complex_nodes, new_reduce_complexity)
 
 
 func _change_visibility_of_group(nodes: Array[Node], new_hidden: bool) -> void:
-	if not nodes or nodes.is_empty():
-		return
+	if not nodes or nodes.is_empty(): return
 	for node: Node in nodes:
-		if new_hidden:
-			_hide_or_disable_node(node)
-		else:
-			_show_or_enable_node(node)
-		
+		_set_node_visibility(node, new_hidden)
 
-func _hide_or_disable_node(node: Node) -> void:
-	if node.has_method("hide"):
-		node.hide()
-	
 
-func _show_or_enable_node(node: Node) -> void:
-	if node.has_method("show"):
-		node.show()
+func _set_node_visibility(node: Node, new_hidden: bool) -> void:
+	var visibility_method_name: String = HIDE_METHOD if new_hidden else SHOW_METHOD 
+	if !hide_recursively and node.has_method(visibility_method_name):
+		node.call(visibility_method_name)
+	else:
+		node.propagate_call(visibility_method_name)
