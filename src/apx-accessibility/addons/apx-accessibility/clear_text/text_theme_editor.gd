dc2afc4e1e10f2952333856e839f32cbaafd9667
kerstin-pc <kerstin@irox-games.com>
2026-01-10 12:40:53 +0100

    apply font color to a specified theme

--- src/apx-accessibility/addons/apx-accessibility/clear_text/text_theme_editor.gd
+++ src/apx-accessibility/addons/apx-accessibility/clear_text/text_theme_editor.gd
@@ -3,6 +3,7 @@ extends Control
 class_name TextThemeEditor
 
 @export var preview_text: Label
+@export var theme_to_edit: Theme
 
 @export_group("Constraints")
 @export var min_size: float = 10
@@ -11,10 +12,14 @@ class_name TextThemeEditor
 @export_group("Fonts")
 @export var fonts: Array[Font]
 
+var style_box: StyleBoxFlat = StyleBoxFlat.new()
+
 
 func _on_font_color_changed(color: Color) -> void:
 	preview_text.add_theme_color_override("font_color", color)
+	theme_to_edit.set_color("font_color", "Label", color)
 
 
 func _on_background_color_changed(color: Color) -> void:
-	print("change background color to ", str(color))
+	style_box.bg_color = color
+	preview_text.add_theme_stylebox_override("normal", style_box)
--- src/apx-accessibility/addons/apx-accessibility/clear_text/text_theme_events.gd
+++ src/apx-accessibility/addons/apx-accessibility/clear_text/text_theme_events.gd
@@ -2,7 +2,7 @@ extends Node
 
 var custom_theme: Theme
 
-signal text_theme_updated
+signal text_theme_updated(Theme)
 
-func notify_theme_update() -> void:
-	text_theme_updated.emit()
+func notify_theme_update(new_theme: Theme) -> void:
+	text_theme_updated.emit(new_theme)
