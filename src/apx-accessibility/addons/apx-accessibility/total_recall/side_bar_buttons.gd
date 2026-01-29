2cec31ba10301d4ceb4bc499206cb13189f92726
kerstin-pc <kerstin@irox-games.com>
2026-01-29 13:26:58 +0100

    fix scrollbar size offsets

--- src/apx-accessibility/addons/apx-accessibility/total_recall/side_bar_buttons.gd
+++ src/apx-accessibility/addons/apx-accessibility/total_recall/side_bar_buttons.gd
@@ -10,11 +10,10 @@ func set_objective_tab(tab: ObjectiveTab) -> void:
 	objective_tab = tab
 
 func create_side_bar_buttons_for_objectives(objectives: Dictionary[String, Objective]) -> void:
+	_remove_all_button_children()
 	if !objectives or objectives.is_empty():
 		push_warning("[Objective Tab] trying to creade sidebar buttons for empty objectives")
-		# TODO: create empty view (or placeholders in scene)
 		return
-	_remove_all_button_children()
 	for objective_key in objectives:
 		append_objective(objective_key, objectives[objective_key])
 	_display_first_objective(objectives)
