4eb0ef0f08698c6dc6149aa28378f9d227917915
kerstin-pc <grin.games@proton.me>
2026-03-10 15:38:52 +0100

    split replacement into smaller functions + test with empty tile

--- src/apx-accessibility/addons/apx-accessibility/clear_channels/simplifiable_tilemap.gd
+++ src/apx-accessibility/addons/apx-accessibility/clear_channels/simplifiable_tilemap.gd
@@ -3,7 +3,8 @@ extends TileMapLayer
 class_name SimplifiableTilemapLayer
 
 const CUSTOM_DATA_COMPLEX: String = "complex"
-const CUSTOM_DATA_COORDS: String = "alternative_coords"
+const CUSTOM_DATA_ORIGINAL_COORDS: String = "original_coords"
+const CUSTOM_DATA_ALT_COORDS: String = "alternative_coords"
 
 func _enter_tree() -> void:
 	if Engine.is_editor_hint():
@@ -13,7 +14,8 @@ func _setup() -> void:
 	if not tile_set:
 		tile_set = TileSet.new()
 	_add_custom_data_layer(CUSTOM_DATA_COMPLEX, TYPE_BOOL)
-	_add_custom_data_layer(CUSTOM_DATA_COORDS, TYPE_VECTOR2I)
+	_add_custom_data_layer(CUSTOM_DATA_ORIGINAL_COORDS, TYPE_VECTOR2I)
+	_add_custom_data_layer(CUSTOM_DATA_ALT_COORDS, TYPE_VECTOR2I)
 
 func _add_custom_data_layer(layer_name: String, type: Variant.Type) -> void:
 	if tile_set.has_custom_data_layer_by_name(layer_name):
@@ -25,18 +27,23 @@ func _add_custom_data_layer(layer_name: String, type: Variant.Type) -> void:
 	tile_set.set_custom_data_layer_type(data_position, type)
 
 
-func _on_hide_complex_changed(hide_complex: bool) -> void:
-	pass
+
 
 func _ready() -> void:
-	_print_custom_data()
+	_on_hide_complex_changed(true)
 
-func _print_custom_data() -> void:
+func _on_hide_complex_changed(hide_complex: bool) -> void:
 	for coords in get_used_cells():
 		var tile_data: TileData = get_cell_tile_data(coords)
-		if not tile_data: continue
-		var tile_is_complex: bool = tile_data.get_custom_data(CUSTOM_DATA_COMPLEX)
-		if tile_is_complex:
-			var alternative_coords: Vector2i = tile_data.get_custom_data(CUSTOM_DATA_COORDS)
-			print("swapping out ", coords, " for ", alternative_coords)
-			set_cell(coords, get_cell_source_id(coords), alternative_coords)
+		if _tile_is_complex(tile_data):
+			_swap_complex_tile(coords, tile_data, hide_complex)
+
+func _tile_is_complex(tile_data: TileData) -> bool:
+	if not tile_data:
+		return false
+	return tile_data.get_custom_data(CUSTOM_DATA_COMPLEX)
+
+
+func  _swap_complex_tile(coords: Vector2i, tile_data: TileData, hide_complex: bool) -> void:
+	var alternative_coords: Vector2i = tile_data.get_custom_data(CUSTOM_DATA_ALT_COORDS)
+	set_cell(coords, get_cell_source_id(coords), alternative_coords)
