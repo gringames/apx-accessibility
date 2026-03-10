31867ebda323440befee1d51740756730c953a46
kerstin-pc <grin.games@proton.me>
2026-03-10 15:31:43 +0100

    swap tiles marked as complex with alternative_coords

--- src/apx-accessibility/addons/apx-accessibility/clear_channels/simplifiable_tilemap.gd
+++ src/apx-accessibility/addons/apx-accessibility/clear_channels/simplifiable_tilemap.gd
@@ -2,16 +2,18 @@
 extends TileMapLayer
 class_name SimplifiableTilemapLayer
 
+const CUSTOM_DATA_COMPLEX: String = "complex"
+const CUSTOM_DATA_COORDS: String = "alternative_coords"
+
 func _enter_tree() -> void:
 	if Engine.is_editor_hint():
 		_setup()
 
 func _setup() -> void:
-	print("setting up")
 	if not tile_set:
 		tile_set = TileSet.new()
-	_add_custom_data_layer("complex", TYPE_BOOL)
-	_add_custom_data_layer("alternative_coords", TYPE_VECTOR2I)
+	_add_custom_data_layer(CUSTOM_DATA_COMPLEX, TYPE_BOOL)
+	_add_custom_data_layer(CUSTOM_DATA_COORDS, TYPE_VECTOR2I)
 
 func _add_custom_data_layer(layer_name: String, type: Variant.Type) -> void:
 	if tile_set.has_custom_data_layer_by_name(layer_name):
@@ -22,5 +24,19 @@ func _add_custom_data_layer(layer_name: String, type: Variant.Type) -> void:
 	tile_set.set_custom_data_layer_name(data_position, layer_name)
 	tile_set.set_custom_data_layer_type(data_position, type)
 
+
 func _on_hide_complex_changed(hide_complex: bool) -> void:
 	pass
+
+func _ready() -> void:
+	_print_custom_data()
+
+func _print_custom_data() -> void:
+	for coords in get_used_cells():
+		var tile_data: TileData = get_cell_tile_data(coords)
+		if not tile_data: continue
+		var tile_is_complex: bool = tile_data.get_custom_data(CUSTOM_DATA_COMPLEX)
+		if tile_is_complex:
+			var alternative_coords: Vector2i = tile_data.get_custom_data(CUSTOM_DATA_COORDS)
+			print("swapping out ", coords, " for ", alternative_coords)
+			set_cell(coords, get_cell_source_id(coords), alternative_coords)
